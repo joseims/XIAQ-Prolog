bce9fb0c9aaec93f32ef8aef6814e0057cab6354
juliobguedes <julio.costa@ccc.ufcg.edu.br>
2018-07-27 14:14:04 -0300

    update

--- main.pl
+++ main.pl
@@ -13,14 +13,18 @@ iniciar_loja(HA, HL, NewHA, NewHL) :-
     (X, Y) = Res,
     NewHA is X, NewHL is Y.
 
+% W is weapon
+% A is armor
 mensagem_loja(W, A, HA, HL, Res) :-
     Msg1 = "Bem Vindo à Loja",
     Msg2 = "Qual item você deseja comprar?",
+    Msg6 = "             Atq, HP",
     Msg3 = "[1] Arma     ",
     Msg4 = "[2] Armadura ",
     Msg5 = "[3] Não vou comprar nada",
     write(Msg1), log(Msg1), log("\n"), nl,
     write(Msg2), log(Msg2), log("\n"), nl,
+    write(Msg6), log(Msg6), log("\n"), nl,
     write(Msg3), log(Msg3),
     write(W), log(W), log("\n"), nl,
     write(Msg4), log(Msg4),
@@ -42,8 +46,10 @@ operacao(_, W, A, HA, HL, Res) :-
 %                               RANKING                              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-inserePontuacao(Pontuacao, Origem, [Pontuacao|Origem]). 
-ordenaRanking(Ranking,Ordenado) :- sort(Ranking, Ordenado).
+ver_recordes(K) :- write(K), nl, mensagem_batalha(K).
+
+inserePontuacao(Pontuacao, Origem, Ordenado) :- K = [Pontuacao|Origem],
+    sort(K, Ordenado).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                             GERADOR ITEM                           %
@@ -84,7 +90,7 @@ alteradorMonstro(Alterer) :-
     getItem(Alteradores, X, Alterer),!. 
 
 aumentaAtributo(X,Y) :- 
-	Y is 1.05*X.
+	Y is 1.032*X.
 
 atualizar_inimigo(HP, ATQ, NewHP, NewATQ) :-
     gera_hp(HP, NewHP), 
@@ -129,49 +135,55 @@ batalha(HA, HL, EA, EL, Coin, Res) :-
     EL / HA < HL / EA -> Res is 1;
     Res is Coin.
 
-fluxo_batalha(HA, HL, EL, EA, Coin, Round) :-
+fluxo_batalha(HA, HL, EL, EA, Coin, Round, K) :-
     monstro(Nome),
     Msg = "Você encontrou um ",
     write(Msg), log(Msg),
     write(Nome), log(Nome), log("\n"), nl,
     atualizar_inimigo(EL, EA, NewEL, NewEA),
     batalha(HA, HL, NewEA, NewEL, Coin, Res),
-    result(Res, HA, HL, NewEA, NewEL, Round).
+    result(Res, HA, HL, NewEA, NewEL, Round, K).
 
-result(Res, HA, HL, EA, EL, Round) :- 
-	Res = 1 -> ganhou(HA, HL, EA, EL, Round);
-	perdeu.
+result(Res, HA, HL, EA, EL, Round, K) :- 
+	Res = 1 -> ganhou(HA, HL, EA, EL, Round, K);
+	perdeu(EL, EA, K).
 
-ganhou(HA,HL,EA, EL, Round) :- 
+ganhou(HA,HL,EA, EL, Round, K) :- 
     Msg = "Parabens você ganhou a luta! Você venceu o Round ", 
     write(Msg), log(Msg),
     soma(1, Round, ProxRound),
     write(Round), log(Round), log("\n\n"), nl, nl,
-    inicio_batalha(HA, HL, EA, EL, ProxRound).
+    inicio_batalha(HA, HL, EA, EL, ProxRound, K).
 
-perdeu :- 
+perdeu(EL, EA, K) :- 
     Msg = "Não foi dessa vez :(",
-    writeln(Msg),
-    log(Msg), log("\n"),
-    main.
+    M2 = "O monstro mais forte que você derrotou tinha: HP: ",
+    M3 = " Ataque: ",
+    writeln(Msg), log(Msg), log("\n"),
+    writeln(M2), write(EL),
+    log(M2), log(EL),
+    write(M3), write(EA),
+    log(M3), log(EA), log("\n"),
+    inserePontuacao(EL, K, Result),
+    mensagem_batalha(Result).
 
-inicio_batalha(HA, HL, EA, EL, 6):- 
+inicio_batalha(HA, HL, EA, EL, 6, K):- 
     Msg = "Você venceu 5 batalhas consecutivas! Vá para a loja melhorar seus equipamentos",
     writeln(Msg), log(Msg), log("\n"),
     iniciar_loja(HA, HL, NewHA, NewHL),
-    inicio_batalha(NewHA, NewHL, EA, EL, 0).
+    inicio_batalha(NewHA, NewHL, EA, EL, 0, K).
 
-inicio_batalha(HA, HL, EA, EL, Round) :-
+inicio_batalha(HA, HL, EA, EL, Round, K) :-
     Msg = "A batalha vai começar!",
     writeln(Msg), log(Msg), log("\n"),
     coin(Res),
-    fluxo_batalha(HA, HL, EA,EL, Res, Round).
+    fluxo_batalha(HA, HL, EA,EL, Res, Round, K).
 
-jogo :-
+jogo(K) :-
 	seleciona_dificuldade(X),
     gerar_heroi(HA, HL, X),
     gerar_inimigo_base(EA, EL),
-    inicio_batalha(HA, HL, EA, EL, 0).
+    inicio_batalha(HA, HL, EA, EL, 0, K).
 
 seleciona_dificuldade(X) :-
     M1 = "Selecione a dificuldade:",
@@ -213,29 +225,29 @@ coin(Resultado) :-
 
 sair.
 
-ver_instrucoes :- 
+ver_instrucoes(K) :- 
     M1 = "O jogo se dá através de sucessivas lutas contra monstros!",
     M2 = "A cada 5 vitórias você poderá ir para loja ficar mais forte!",
     M3 = "Dê o seu melhor para obter o maior numero de pontos possíveis!",
 	writeln(M1), log(M1), log("\n"),
 	writeln(M2), log(M2), log("\n"),
 	writeln(M3), log(M3), log("\n\n"), nl,
-	main.
+	mensagem_batalha(K).
 
-redireciona(1) :- jogo.
-redireciona(2) :- ver_instrucoes.
-redireciona(3) :- ver_recordes.
-redireciona(4) :- sair.
-redireciona(_) :- 
+redireciona(1, K) :- jogo(K).
+redireciona(2, K) :- ver_instrucoes(K).
+redireciona(3, K) :- ver_recordes(K).
+redireciona(4, _) :- sair.
+redireciona(_, _) :- 
     M1 = "Entrada Inválida. Insira outro valor.",
     write(M1), log(M1), log("\n"), nl,
-    mensagem_batalha().
+    mensagem_batalha(K).
 
 mensagem_inicial() :-
     nl, M1 = "\nBem Vindo ao XIAQ",
     write(M1), log(M1), log("\n"), nl.
 
-mensagem_batalha() :- 
+mensagem_batalha(K) :- 
     M1 = "[1] Iniciar batalha",
     M2 = "[2] Ver Instruções",
     M3 = "[3] Visualizar Recordes",
@@ -244,7 +256,7 @@ mensagem_batalha() :-
     write(M2), log(M2), log("\n"), nl,
     write(M3), log(M3), log("\n"), nl,
     write(M4), log(M4), log("\n\n"), nl, nl,
-    read(Entrada), log(Entrada), log("\n"), nl, redireciona(Entrada).
+    read(Entrada), log(Entrada), log("\n"), nl, redireciona(Entrada, K).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                LOG                                 %
@@ -259,4 +271,4 @@ log(Text) :-
 %                                MAIN                                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-main :- mensagem_inicial(), mensagem_batalha().
+main :- mensagem_inicial(), K = [], mensagem_batalha(K).
